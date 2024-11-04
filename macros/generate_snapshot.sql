d8c5fb161fb5debccc62ca0eea6a85391faebbff
lony-priya02 <lony.priya@ibm.com>
2024-11-04 16:07:32 +0530

    code changes

--- macros/generate_snapshot.sql
+++ macros/generate_snapshot.sql
@@ -1,14 +1,14 @@
 {% macro generate_snapshot(snapshot_name, source_schema, source_table, unique_key, updated_at) %}
-{% snapshot {{ sat_order_snapshot }} %}
     
     {{ config(
-materialized='snapshot',
-unique_key='C_CUSTKEY',
-strategy='timestamp',
-updated_at='UPDATE_DT',
-target_schema=generate_schema_name('snapshots')
+        materialized='snapshot',
+        unique_key='o_orderkey',
+        strategy='timestamp',
+        updated_at='updated_at',
+        target_schema=generate_schema_name('snapshots')
     ) }}
 
-select * from {{ source(source_schema, source_table) }}
-{% endfor %}
+    select * from {{ source(source_schema, source_table) }}
+
+{% endsnapshot %}
 {% endmacro %}
